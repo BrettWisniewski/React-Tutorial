6afc69ed58fee11e8ff43ce596e5f40c04ec96d5
Brett Wisniewski <bpw10va@gmail.com>
2022-07-19 23:48:43 -0600

    images showing up

--- basic/src/App.js
+++ basic/src/App.js
@@ -2,13 +2,15 @@
 import './App.css';
 import shopdata from "./dataforshop.js"
 
+// price might have trouble showing up since it is not a string, test this
+
 function ThingsTest(props){
   return(
     <div>
       <h3>
       {props.name}
       </h3>
-      <img src = {`.../basic/${props.pic}`} />
+      <img src = {`/images/${props.pic}`} />
     </div>
   )
 }
@@ -19,6 +21,7 @@ function App() {
     <div className="App">
       <p> Some text as a test to see if it works</p>
       <p> Another test</p>
+      <p> WILL THE IMAGES SHOW UP PART 2?</p>
       {shopdata.map((item)=>{
        return (
        <ThingsTest key = {item.id} name={item.title} cost={item.price} pic = {item.picture}/>
