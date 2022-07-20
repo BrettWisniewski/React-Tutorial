1d0ef172d0f9ba05bfcfdd38de53cff9e3fb195b
Brett Wisniewski <bpw10va@gmail.com>
2022-07-19 23:20:39 -0600

    testing image

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,5 +1,17 @@
 
 import './App.css';
+import shopdata from "./dataforshop.js"
+
+function ThingsTest(props){
+  return(
+    <div>
+      <h3>
+      {props.name}
+      </h3>
+      <img src = {`.../basic/${props.pic}`} />
+    </div>
+  )
+}
 
 function App() {
   // Test of New Branch for New Site
@@ -7,6 +19,10 @@ function App() {
     <div className="App">
       <p> Some text as a test to see if it works</p>
       <p> Another test</p>
+      {shopdata.map((item)=>{
+       return (
+       <ThingsTest key = {item.id} name={item.title} cost={item.price} pic = {item.picture}/>
+     );})}
     </div>
   );
 }
