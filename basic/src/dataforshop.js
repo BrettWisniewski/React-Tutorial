e10415cd1934c99ba85ceb18bc5f6a53ccabbef1
Brett Wisniewski <bpw10va@gmail.com>
2022-07-19 22:59:26 -0600

    See if Github Publishes

--- basic/src/dataforshop.js
+++ basic/src/dataforshop.js
@@ -1,6 +1,16 @@
 const shopdata = [
     {
         id : 1,
-        
-    }
+        title: "React Logo 2",
+        price: 30,
+        picture : "logo192.png"
+        // Don't think you do {} here, shop the very best deals!- money sign animation
+    },
+    {
+        id: 2,
+        title: "React Logo 2",
+        price: 40,
+        picture : "logo512.png"
+      }
 ]
+export default shopdata;
